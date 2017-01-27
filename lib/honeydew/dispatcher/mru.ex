5086b770f96c14f4b1821aeab25f37db151e04e7
Michael Shapiro <koudelka@ryoukai.org>
2017-01-26 17:34:05 -0800

    remove debug statements from dispatchers

--- lib/honeydew/dispatcher/mru.ex
+++ lib/honeydew/dispatcher/mru.ex
@@ -1,6 +1,4 @@
 defmodule Honeydew.Dispatcher.MRU do
-  require Logger
-
   # TODO: docs
   # TODO: abstract common LRU/MRU functionality?
 
@@ -13,23 +11,19 @@ defmodule Honeydew.Dispatcher.MRU do
   end
 
   def check_in(worker, free) do
-    Logger.debug "[Honeydew] #{inspect self()} checked in worker #{inspect worker}"
     :queue.in_r(worker, free)
   end
 
   def check_out(_job, free) do
     case :queue.out(free) do
       {{:value, worker}, free} ->
-        Logger.debug "[Honeydew] Queue #{inspect self()} checked out worker #{inspect worker}"
         {worker, free}
       {:empty, _free} ->
-        Logger.debug "[Honeydew] Queue #{inspect self()} is out of workers"
         {nil, free}
     end
   end
 
   def remove(worker, free) do
-    Logger.debug "[Honeydew] Queue #{inspect self()} removing worker #{inspect worker}"
     :queue.filter(&(&1 != worker), free)
   end
 end
