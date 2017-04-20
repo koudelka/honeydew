96e270c76c414517f77bdc544fafe0dd832eff4a
Michael Shapiro <koudelka@ryoukai.org>
2017-04-20 12:42:20 -0700

    hush intentional crashes in tests, thanks @aaronrenner

--- test/honeydew/failure_mode/requeue_test.exs
+++ test/honeydew/failure_mode/requeue_test.exs
@@ -1,5 +1,6 @@
 defmodule Honeydew.FailureMode.RequeueTest do
   use ExUnit.Case, async: true
+  import ExUnit.CaptureLog
 
   setup do
     queue = :erlang.unique_integer
@@ -13,25 +14,29 @@ defmodule Honeydew.FailureMode.RequeueTest do
   end
 
   test "should requeue the job on the new queue", %{queue: queue, failure_queue: failure_queue} do
-    {:crash, [self()]} |> Honeydew.async(queue)
-    assert_receive :job_ran
+    capture_log(fn ->
+      {:crash, [self()]} |> Honeydew.async(queue)
+      assert_receive :job_ran
 
-    Process.sleep(500) # let the failure mode do its thing
+      Process.sleep(500) # let the failure mode do its thing
 
-    assert Honeydew.status(queue) |> get_in([:queue, :count]) == 0
-    refute_receive :job_ran
+      assert Honeydew.status(queue) |> get_in([:queue, :count]) == 0
+      refute_receive :job_ran
 
-    assert Honeydew.status(failure_queue) |> get_in([:queue, :count]) == 1
+      assert Honeydew.status(failure_queue) |> get_in([:queue, :count]) == 1
+    end)
   end
 
   test "should inform the awaiting process of the error", %{queue: queue, failure_queue: failure_queue} do
-    job = {:crash, [self()]} |> Honeydew.async(queue, reply: true)
+    capture_log(fn ->
+      job = {:crash, [self()]} |> Honeydew.async(queue, reply: true)
 
-    assert {:requeued, {%RuntimeError{message: "ignore this crash"}, _stacktrace}} = Honeydew.yield(job)
+      assert {:requeued, {%RuntimeError{message: "ignore this crash"}, _stacktrace}} = Honeydew.yield(job)
 
-    {:ok, _} = Helper.start_worker_link(failure_queue, Stateless)
+      {:ok, _} = Helper.start_worker_link(failure_queue, Stateless)
 
-    # job ran in the failure queue
-    assert {:error, {%RuntimeError{message: "ignore this crash"}, _stacktrace}} = Honeydew.yield(job)
+      # job ran in the failure queue
+      assert {:error, {%RuntimeError{message: "ignore this crash"}, _stacktrace}} = Honeydew.yield(job)
+    end)
   end
 end
