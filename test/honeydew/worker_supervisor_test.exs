1cb1ec0fb72aa833381ca5739b8d3995b5924fee
Aaron Renner <aaron.renner@gmail.com>
2017-11-29 14:02:23 -0700

    Propagate error upward if unable to start worker
    I was having an issue where if I typo-ed my worker module name, so I'd
    have a worker pool start up, but with no workers. Since the number of
    workers aren't changing at runtime, I found I could just create one
    child in the WorkerSupervisor for each child, and not have to use a
    separate start_child function to get everything started. Since we're now
    using a :one_for_one strategy, it makes it easy for
    WorkerSupervisor.start_link to return a standard :failure_to_start_child
    error tuple.

--- test/honeydew/worker_supervisor_test.exs
+++ test/honeydew/worker_supervisor_test.exs
@@ -1,22 +1,30 @@
 defmodule Honeydew.WorkerSupervisorTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+  alias Honeydew.WorkerSupervisor
 
   setup do
     pool = :erlang.unique_integer
-
     Honeydew.create_groups(pool)
 
-    {:ok, supervisor} = Honeydew.WorkerSupervisor.start_link(pool, Stateful, [:state_here], 7, 5, 10_000)
+    on_exit fn ->
+      Honeydew.delete_groups(pool)
+    end
 
-    # on_exit fn ->
-    #   Supervisor.stop(supervisor)
-    #   Honeydew.delete_groups(pool)
-    # end
-
-    [supervisor: supervisor]
+    [pool: pool]
   end
 
-  test "starts the correct number of workers", %{supervisor: supervisor} do
+  test "starts the correct number of workers", %{pool: pool} do
+    {:ok, supervisor} =
+      WorkerSupervisor.start_link(pool, Stateful, [:state_here], 7, 5, 10_000)
+
     assert supervisor |> Supervisor.which_children |> Enum.count == 7
+  end
+
+  test "crashes when the worker module does not exist", %{pool: pool} do
+    Process.flag(:trap_exit, true)
+
+    assert {:error, {:shutdown, {:failed_to_start_child, _, _}}} =
+      WorkerSupervisor.start_link(pool, DoesNotExist, [], 7, 5, 10_000)
   end
 end
