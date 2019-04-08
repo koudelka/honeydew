dfd22e95f7fdb6a3d9e9dee04370ed3839bf0eab
Michael Shapiro <koudelka@ryoukai.org>
2019-04-08 13:59:32 -0700

    multi time warp travis/hammer

--- test/hammer/hammer.sh
+++ test/hammer/hammer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mix compile.protocols
-elixir -pa _build/$MIX_ENV/consolidated  --erl "+P 134217727" -S mix run `dirname $0`/hammer.exs
+elixir -pa _build/$MIX_ENV/consolidated  --erl "+P 134217727 +C multi_time_warp" -S mix run `dirname $0`/hammer.exs
