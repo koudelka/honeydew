77dabeb401f4301d72e45250d912207157762021
Michael Shapiro <koudelka@ryoukai.org>
2021-05-16 16:03:23 -0700

    bump deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Honeydew.Mixfile do
     [
       {:ecto, "~> 3.0", optional: true, only: [:dev, :prod]},
       {:ex_doc, ">= 0.0.0", only: :dev},
-      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       # {:eflame, git: "git@github.com:slfritchie/eflame", only: :dev},
     ]
   end
